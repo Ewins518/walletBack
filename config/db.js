521781c8e43e0cac6c3defac6514154d43d2e37f
Mr Ewins <ewins@localhost.localdomain>
2021-08-27 10:26:10 +0200

    heroku deployment

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
+    HOST: "ec2-44-196-8-220",
     PORT: "5432",
-    USER: "ewins",
-    PASSWORD: "ewins",
-    DB: "wallet",
+    USER: "lzubvniygddmhj",
+    PASSWORD: "8ef2bc9d0bef4a34ea5511b28269955ca12b2955ed6a9fab4815e6abd4355732",
+    DB: "d1cpne62oir63",
     dialect: "postgres",
 }
